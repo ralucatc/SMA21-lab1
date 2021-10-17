1776375a5f364912991f53e0c81083bc38b2250f
Raluca <carare.raluca27@gmail.com>
2021-10-18 00:21:36 +0300

    Lab2

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -1,18 +1,29 @@
 package com.example.lab1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Spinner;
+import android.widget.Toast;
+import com.example.lab1.R;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     EditText eName;
     Button button;
-    TextView tName;
+    Button bShare;
+    Button bSearch;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +32,97 @@ public class MainActivity extends AppCompatActivity {
 
         eName = (EditText) findViewById(R.id.eName);
         button = (Button) findViewById(R.id.button);
-        tName = (TextView) findViewById(R.id.tName);
+        bShare = (Button) findViewById(R.id.bShare);
+        bSearch = (Button) findViewById(R.id.bSearch);
+
+        // spinner
+        spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.colors,
+                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View myView) {
+            public void onClick(View v) {
                 String name = eName.getText().toString();
-                tName.setText(String.format("Bună, %s!", name));
+                dialog(name);
             }
         });
+
+        bShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Share(v);
+            }
+        });
+
+        bSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Search(v);
+            }
+        });
+    }
+
+    private void dialog(String name) {
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).setTitle("This is the dialog")
+                .setMessage(String.format("Buna %s", name))
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "The message has been generated", Toast.LENGTH_SHORT).show();
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "Dialog has been closed", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                }).create();
+
+        alertDialog.show();
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String color = parent.getItemAtPosition(position).toString();
+
+        switch (color) {
+            case "Red":
+                button.setTextColor(Color.RED);
+            case "Yellow":
+                button.setTextColor(Color.YELLOW);
+            case "Blue":
+                button.setTextColor(Color.BLUE);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public void Share(View view) {
+        String name = ((EditText) findViewById(R.id.eName)).getText().toString();
+
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, name);
+        sendIntent.setType("text/plain");
+
+        Intent shareIntent = Intent.createChooser(sendIntent, null);
+        startActivity(shareIntent);
+    }
+
+    public void Search(View view) {
+        String name = ((EditText) findViewById(R.id.eName)).getText().toString();
+        String url = "https://www.google.com/search?q=" + name;
+
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
     }
 }
